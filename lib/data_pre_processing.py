e4aa24d3077829123fc6a73b4471903fd062e63b
Nikita Acharya <nikita.acharya8@gmail.com>
2021-04-24 14:30:31 -0400

    Add function to load the feature and graph

--- lib/data_pre_processing.py
+++ lib/data_pre_processing.py
@@ -1,21 +1,68 @@
-import glob
-import os
-import pdb
+import numpy as np
+import pickle as pkl
+import networkx as nx
+import scipy.sparse as sp
+from scipy.sparse.linalg.eigen.arpack import eigsh
 import sys
-
-DATA_FOLDER = "nepali_news_dataset_20_categories_large"
-
-def load_data(data_folder=DATA_FOLDER):
-  sys.path.append('../data')
-  data = []
-  full_path = f"../data/{data_folder}/**/*.txt"
-  print(f"Loading file from: {full_path}")
-  for categorized_article in glob.iglob(full_path, recursive=True): 
-      category = categorized_article.split("/")[-2]
-      # data[category] = data.get(category, [])
-      with open(categorized_article, encoding="utf8", errors='ignore') as file:
-        article = file.read()
-        data.append([category, article])
-  return data
+import pdb
 
 
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+
+def load_data(dataset_str, path_to_data="data"):
+    """Load data."""
+    names = ["x", "y", "tx", "ty", "allx", "ally", "graph"]
+    objects = []
+    for i in range(len(names)):
+        with open(f"{path_to_data}/{dataset_str}.{names[i]}", "rb") as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding="latin1"))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file(
+        "{}/{}.test.index".format(path_to_data, dataset_str)
+    )
+
+    test_idx_range = np.sort(test_idx_reorder)
+
+    features = sp.vstack((allx, tx)).tolil()
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    idx_test = test_idx_range.tolist()
+    idx_train = range(len(y))
+    idx_val = range(len(y), len(y) + 500)
+
+    print(adj.shape)
+    print(features.shape)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def preprocess_features(features):
+    """Row-normalize feature matrix and convert it to dense representation"""
+    rowsum = np.array(features.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.0
+    r_mat_inv = sp.diags(r_inv)
+    features = r_mat_inv.dot(features)
+    return features.todense()
+
+
+def load_path(path):
+    sys.path.append(path)
+
+
+def remove_path(path):
+    sys.path.remove(path)
