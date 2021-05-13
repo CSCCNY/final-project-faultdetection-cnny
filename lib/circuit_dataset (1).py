e64bdb0e7ecb166c28c1901bc3d3f2b8e5995f3a
Saurav Pradhan <sauravpradhan93@gmail.com>
2021-05-13 10:58:57 -0400

    Updated input parameters

---
+++ lib/circuit_dataset (1).py
@@ -0,0 +1,31 @@
+from spektral.data import Dataset, DisjointLoader, Graph
+from data_pre_processing import load_data
+
+
+class CircuitDataset(Dataset):
+    def read(self):
+        circuits = []
+        # circs = [
+        #     "c6288",
+        #     "c5315",
+        #     "c432",
+        #     "c499",
+        #     "c880",
+        #     "c1355",
+        #     "c1908",
+        #     "c3540",
+        #     "adder.bench",
+        #     "arbiter.bench",
+        #     "cavlc.bench",
+        #     "dec.bench",
+        #     "voter.bench",
+        #     "sin.bench",
+        #     "priority.bench",
+        # ]
+        path = self.path if self.path else "../data/output"
+        circs = self.circs if self.circs else []
+        for circ in circs:
+            A, X, labels = load_data(circ, path, normalize="")
+            circuits.append(Graph(x=X.toarray(), a=A, y=labels))
+            print(f"{circ}: {sum(labels)}, {len(labels)}")
+        return circuits
